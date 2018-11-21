8b1a35390ac92149729aebf50c9a94080f89a6e5
孟祥钊 <1090316751@qq.com>
2018-11-21 17:38:15 +0800

    完成RecyclerView的封装

--- app/src/main/java/me/maxandroid/italker/MainActivity.java
+++ app/src/main/java/me/maxandroid/italker/MainActivity.java
@@ -1,13 +1,22 @@
 package me.maxandroid.italker;
 
-import android.support.v7.app.AppCompatActivity;
-import android.os.Bundle;
+import android.widget.TextView;
 
-public class MainActivity extends AppCompatActivity {
+import butterknife.BindView;
+import me.maxandroid.common.app.Activity;
+
+public class MainActivity extends Activity {
+    @BindView(R.id.txt_test)
+    TextView mTestText;
 
     @Override
-    protected void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+    protected int getContentLayoutId() {
+        return R.layout.activity_main;
+    }
+
+    @Override
+    protected void initWidget() {
+        super.initWidget();
+        mTestText.setText("test");
     }
 }
