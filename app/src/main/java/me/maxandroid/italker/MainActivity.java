14897afabd0fd0fbe3f8b7e17b6d86bd7e4808fc
mxz <1090316751@qq.com>
2018-11-22 16:34:01 +0800

    完成主界面的外层搭建

--- app/src/main/java/me/maxandroid/italker/MainActivity.java
+++ app/src/main/java/me/maxandroid/italker/MainActivity.java
@@ -1,13 +1,35 @@
 package me.maxandroid.italker;
 
+import android.support.design.widget.BottomNavigationView;
+import android.view.View;
+import android.widget.FrameLayout;
 import android.widget.TextView;
 
+import com.bumptech.glide.Glide;
+import com.bumptech.glide.load.resource.drawable.GlideDrawable;
+import com.bumptech.glide.request.animation.GlideAnimation;
+import com.bumptech.glide.request.target.ViewTarget;
+
 import butterknife.BindView;
+import butterknife.OnClick;
 import me.maxandroid.common.app.Activity;
+import me.maxandroid.common.widget.PortraitView;
 
 public class MainActivity extends Activity {
-    @BindView(R.id.txt_test)
-    TextView mTestText;
+    @BindView(R.id.appbar)
+    View mLayAppbar;
+
+    @BindView(R.id.im_portrait)
+    PortraitView mPortrait;
+
+    @BindView(R.id.txt_title)
+    TextView mTitle;
+
+    @BindView(R.id.lay_container)
+    FrameLayout mContainer;
+
+    @BindView(R.id.navigation)
+    BottomNavigationView mNavigation;
 
     @Override
     protected int getContentLayoutId() {
@@ -17,6 +39,24 @@ public class MainActivity extends Activity {
     @Override
     protected void initWidget() {
         super.initWidget();
-        mTestText.setText("test");
+        Glide.with(this)
+                .load(R.drawable.bg_src_morning)
+                .centerCrop()
+                .into(new ViewTarget<View, GlideDrawable>(mLayAppbar) {
+                    @Override
+                    public void onResourceReady(GlideDrawable resource, GlideAnimation<? super GlideDrawable> glideAnimation) {
+                        this.view.setBackground(resource.getCurrent());
+                    }
+                });
+    }
+
+    @OnClick(R.id.im_search)
+    void onSearchMenuClick() {
+
+    }
+
+    @OnClick(R.id.btn_action)
+    void onActionClick() {
+
     }
 }
